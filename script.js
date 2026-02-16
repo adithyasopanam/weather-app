dcfbbcb7984b8779305b6b68ae98b7397952236b
Adithya S <adu9563@gmail.com>
2026-02-16 12:33:21 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function fun(){
+    let city=document.getElementById("city").value;
+    if(city==""){
+        alert("Please enter a city name");
+        return false;
+    }
+    else{
+        document.getElementById("output").innerHTML=city.toUpperCase();
+        let date=new Date();
+        document.getElementById("date").innerHTML=date;
+    }
+}
